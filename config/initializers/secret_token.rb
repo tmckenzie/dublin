c6a30f76dfc5976c57d4c0508ace0d17cfae0e7b
Tim <tim@Tim-McKenzies-MacBook.local>
2011-12-09 20:03:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dublin::Application.config.secret_token = 'a34e0830bfa70389b24556e0958bf6bcb3323c166c2bec809391a3e9a74ce4de3c1e0bd1d2aede4a1fcd998b2ed16e653705fbbcea3af7fd3009966ce1ad3987'
